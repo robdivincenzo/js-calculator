3a31a5620c31349437011e176e8aeeafca9fc744
Rob DiVincenzo <rob.divincenzo@gmail.com>
2022-07-17 01:28:01 -0400

    Create DOM functions
    Change display on number click
    Store display value for operating on operator click

--- main.js
+++ main.js
@@ -1,3 +1,46 @@
+/* Globals */
+let numberButtons = document.querySelectorAll("button.number");
+let inputButtons = document.querySelectorAll("button.input");
+let display = document.querySelector("span.result");
+let clearButton = document.querySelector("button.clear");
+let operand1 = "";
+let calculatingFlag = false;
+
+/* Event handlers */
+numberButtons.forEach( (numberButton) => {
+    numberButton.addEventListener( 'click', addNumberToDisplay);
+});
+
+clearButton.addEventListener( 'click', clearDisplay );
+
+inputButtons.forEach( (inputButton) => {
+    inputButton.addEventListener( 'click', function(e) {
+        if( display.innerText != "" ) {
+            operand1 = Number(display.innerText);
+            calculatingFlag = true;
+        }
+    });
+});
+
+
+/* DOM functions */
+function addNumberToDisplay(e) {
+    if( calculatingFlag == true ) {
+        display.innerText = "";
+        calculatingFlag = false;
+    }
+    if( display.innerText == 0 ) {
+        display.innerText = "";
+    }
+    display.innerText += e.target.innerText;
+}
+
+function clearDisplay(e) {
+    display.innerText = "";
+}
+
+
+/* Operations functions */
 function add(num1, num2) {
     return num1+num2;
 }
@@ -33,3 +76,5 @@ function operate(operator, num1, num2) {
             break;
     }
 }
+
+
